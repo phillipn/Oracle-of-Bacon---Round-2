5db8c0558f085a5977bb94dc00ad67dce73d470c
Nick Phillips <phillipn101@gmail.com>
2016-06-16 19:31:51 -0500

    Finished

---
+++ lib/oracle_of_bacon.rb
@@ -0,0 +1,91 @@
+require 'byebug'                # optional, may be helpful
+require 'open-uri'              # allows open('http://...') to return body
+require 'cgi'                   # for escaping URIs
+require 'nokogiri'              # XML parser
+require 'active_model'          # for validations
+
+class OracleOfBacon
+
+  class InvalidError < RuntimeError ; end
+  class NetworkError < RuntimeError ; end
+  class InvalidKeyError < RuntimeError ; end
+
+  attr_accessor :from, :to
+  attr_reader :api_key, :response, :uri
+  
+  include ActiveModel::Validations
+  validates_presence_of :from
+  validates_presence_of :to
+  validates_presence_of :api_key
+  validate :from_does_not_equal_to
+
+
+  def initialize(api_key='', to='Kevin Bacon', from='Kevin Bacon')
+    @from = from 
+    @to = to
+    @api_key = api_key
+  end
+  
+  def from_does_not_equal_to
+    errors.add(:from, "cannot be equal to to") if from == to
+  end
+
+  def find_connections
+    make_uri_from_arguments
+    begin
+      xml = URI.parse(uri).read
+      Response.new(xml)
+    rescue 
+      raise NetworkError
+    end
+    # your code here: create the OracleOfBacon::Response object
+  end
+
+  def make_uri_from_arguments
+    @uri = "http://oracleofbacon.org/cgi-bin/xml?p=#{CGI.escape(@api_key)}&a=#{CGI.escape(@from)}&b=#{CGI.escape(@to)}"
+  end
+      
+  class Response
+    attr_reader :type, :data
+    # create a Response object from a string of XML markup.
+    def initialize(xml)
+      @doc = Nokogiri::XML(xml)
+      parse_response
+    end
+
+    private
+
+    def parse_response
+      if ! @doc.xpath('/error').empty?
+        parse_error_response
+      elsif ! @doc.xpath('/link').empty?
+        parse_graph_response
+      elsif ! @doc.xpath('/spellcheck').empty?
+        parse_spellcheck_response
+      else
+        parse_unknown_response       
+      end
+    end
+    
+    def parse_error_response
+      @type = :error
+      @data = 'Unauthorized access'
+    end
+    
+    def parse_graph_response
+      @type = :graph
+      @data = @doc.xpath('/link/actor').zip(@doc.xpath('/link/movie')).flatten.compact.map(&:text)
+    end
+    
+    def parse_spellcheck_response
+      @type = :spellcheck
+      @data = @doc.xpath('/spellcheck/match').map(&:text)
+    end
+    
+    def parse_unknown_response
+      @type = :unknown
+      @data = 'Unknown Issue'
+    end
+  end
+end
+
